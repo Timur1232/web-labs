3a710146244906c1deff06f0605230c320fa238d
timur <timtimch123@gmail.com>
2026-02-06 21:54:13 +0300

    added lab1 tour

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php
 
-declare(strict_types = 1);
+//////////////////////////////////////////////////////////////////////////
+// ============================ Lab 1 Tour ============================ //
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// 1. Start                     - index.php                             //
+// 2. Router class              - app/core/router.php:15                //
+// 3. RouteGroup class          - app/core/router.php:101               //
+// 4. Request class             - app/core/request.php:51               //
+// 5. URL class                 - app/core/request.php:5                //
+// 6. View class                - app/core/view.php:31                  //
+// 7. Layout function           - app/core/layout.php:25                //
+// 8. Helper functions          - app/core/helpers.php:19               //
+// 9. Controllers               - app/controllers/index.php:10          //
+//                              - app/controllers/about_me.php:10       //
+//                              - app/controllers/interests.php:12      //
+//                              - app/controllers/study.php:14          //
+//                              - app/controllers/photoalbum.php:13     //
+//                              - app/controllers/callback.php:16       //
+//                              - app/controllers/history.php:10        //
+// 10. FormValidator class      - app/core/data_validator.php:242       //
+// 11. DataValidator class      - app/core/data_validator.php:22        //
+// 12. Photoalbum model         - app/models/photoalbum.php:18          //
+// 13. Interests model          - app/models/interests.php:9            //
+// 14. Interests model instance - app/models/instances/interests.php:17 //
+// 15. Test results model       - app/models/test_result.php:9          //
+// 16. Callback validator model - app/models/callback_validator.php:9   //
+// 17. Photoalbum view          - app/views/photoalbum.php:11           //
+// 18. Callback view            - app/views/callback.php:9              //
+// 19. Templates                                                        //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
 
 require_once 'app/core/core.php';
 require_once 'app/controllers/controllers.php';
